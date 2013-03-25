3d34e4ac8d5c974753387baf5491c17f857d92c2
Khalasar <andre_stuhrmann@web.de>
2013-03-25 20:05:05 +0100

    add test-methods: test_check_action; test_is_winner/ and add methods check_action, action

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,10 +1,22 @@
 module ConnectFour
 
-  #require './player'
-  require 'player'
+  require './player'
+  #require 'player'
 
-  def self.hello_world
-    "hello world"
+  def self.start_game
+    @player1 = add_player('x')
+    @player2 = add_player('o')
+
+    create_playing_area
+    @changer =0
+    @active = @player1
+    while(is_winner) do
+      show_playing_area()
+      puts
+      print "Ihr Eingabe bitte Player #{@active.name}:"
+      input=gets
+      @active = action @active.name,input.to_i
+    end
   end
 
   def self.add_player(name)
@@ -14,36 +26,61 @@ module ConnectFour
   def self.create_playing_area
     rows=8
     cols=8
-    @playing_area = Array.new(cols,'-') {Array.new(rows,'-')}
+    @playing_area = Array.new(cols,'.') {Array.new(rows,'.')}
   end
 
-  @player1 = add_player('x')
-  @player2 = add_player('o')
-
-  def self.start_game
-
-    while(!is_winner) do
-      print "Ihr Eingabe bitte Player #{active_player.name}:"
-      eingabe=gets
+  def self.show_playing_area
+    puts
+    puts '12345678'
+    x=@playing_area.size-1
+    while x>=0 do
+      y=0
+      while y<@playing_area[0].size do
+        print @playing_area[x][y]
+        y = y+1
+      end
+      puts
+      x = x-1
     end
+
   end
 
   def self.active_player
-    changer = 1
-    if changer ==1
+
+    if @changer ==1
       @player1
-      changer=0
+      @changer=0
       active = @player1
     else
       @player2
-      changer=1
+      @changer=1
       active = @player2
     end
   active
   end
 
   def self.action(player_sign, col)
-    @playing_area[col-1][0]=player_sign
+    if check_action col
+    @playing_area[0][col-1]=player_sign
+      active_player
+    else
+         puts 'Falscher Spielzug!'
+           @active
+    end
   end
+
+  def self.check_action(col)
+    if col>0 && col <9
+      true
+    else
+      false
+    end
+  end
+
+  def self.is_winner
+    true
+  end
+
+  start_game
 
 end
--- test/connect_four_test.rb
+++ test/connect_four_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class ConnectFourTest < ConnectFourSpec
-  def test_hello_world
-    assert_equal "hello world", ConnectFour.hello_world
-  end
-
   def test_add_player
     assert_equal 'x', ConnectFour.add_player('x').name
   end
@@ -26,19 +22,23 @@ class ConnectFourTest < ConnectFourSpec
     player = ConnectFour.add_player('x')
     ConnectFour.action(player.name,2)                       # player(x) place a x in column 2
 
-    assert_equal 'x', playing_area[1][0]          # 1 is the 2nd column because an array starts at 0
+    assert_equal 'x', playing_area[0][1]          # 1 is the 2nd column because an array starts at 0
   end
 
-  #
-  #def test_is_winner
-  #  playing_area = ConnectFour.create_playing_area
-  #  player1 = ConnectFour.add_player('x')
-  #  player2 = ConnectFour.add_player('o')
-  #  player1.action_player(2)                       # player(x) place a x in column 2
-  #  player1.action_player(3)
-  #  player1.action_player(4)
-  #  player1.action_player(5)
-  #  assert_equal player1, ConnectFour.is_winner
-  #end
+  def test_is_winner
+    playing_area = ConnectFour.create_playing_area
+    player1 = ConnectFour.add_player('x')
+    player2 = ConnectFour.add_player('o')
+    ConnectFour.action(player1,2)                       # player(x) place a x in column 2
+    ConnectFour.action(player1,3)
+    ConnectFour.action(player1,4)
+    ConnectFour.action(player1,5)
+    assert_equal player1, ConnectFour.is_winner
+  end
+
+  def test_check_action
+    assert_equal false, ConnectFour.check_action(9)
+
+  end
 
 end
